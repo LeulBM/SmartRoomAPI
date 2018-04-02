1ad372394ccd8573f38c1ca15f84776c4d85cf89
LeulBM <leul.berhanemeskel@gmail.com>
2018-04-01 22:33:43 -0400

    added very basic error handling

--- app/Http/Controllers/ShadesController.php
+++ app/Http/Controllers/ShadesController.php
@@ -5,25 +5,35 @@ namespace App\Http\Controllers;
 use App\Author;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception;
 
 class ShadesController extends Controller{
 	public $http;
 
 	public function __construct() {
-		$this->http = new Client(['base_uri' => 'https://agrpi.csh.rit.edu:5000/gpio/']);
+		$this->http = new Client(['base_uri' => 'https://agrpi.csh.rit.edu:5000/gpio/',
+					'timeout' => 5,
+					'connect_timeout'=> 5]);
 	}
 
 	public function getCurrent() {
-		$response = $this->http->get('get/current');
-		return response()->json(json_decode($response->getBody()),$response->getStatusCode());
+		try{
+			$response = $this->http->get('get/current');
+			return response()->json(json_decode($response->getBody()),$response->getStatusCode());
+		} catch(\Exception $e) {
+			die($e->getMessage());
+		}
 	}
 
 	public function setHeight(Request $request) {
 		$this->validate($request, [
 			'howOpen' => 'required|numeric|min:0|max:100'
 		]);
-		$url = $request->howOpen.'/';
+		try {
 		$response = $this->http->post($request->howOpen);
+		} catch(\Exception $e) {
+			die($e->getMessage());
+		}
 		return response()->json(json_decode($response->getBody()),$response->getStatusCode());
 	}
 }
